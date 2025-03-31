6c993377e259a290665294ea98f845ed68fd917d
butadpj <butadpj@gmail.com>
2025-04-01 00:05:56 +0800

    Fix date issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { warn } from "node_modules/astro/dist/core/logger/core";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +11,11 @@ export function calculateReadingTime(text: string) {
   const wordCount = text.split(/\s+/).length;
   const minutes = Math.ceil(wordCount / wordsPerMinute);
   return minutes;
+}
+
+export function formatDate(date: Date) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+
+  //@ts-ignore
+  return date.toLocaleDateString("en-US", options);
 }
